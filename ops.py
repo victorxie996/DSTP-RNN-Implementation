4e6cba40639f2c75921dfc2f835556aac8a6418b
Victor Xie <56447006+victorxie996@users.noreply.github.com>
2020-11-25 23:27:40 +0000

    Add files via upload

---
+++ ops.py
@@ -0,0 +1,36 @@
+"""Helper functions.
+
+@author Zhenye Na 05/21/2018
+
+"""
+
+
+# from tqdm import tqdm
+# import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+
+
+# import matplotlib
+# matplotlib.use('Agg')
+
+
+def read_NDX(input_path, debug=True):
+    """Read nasdaq stocks data.
+
+    Args:
+        input_path (str): directory to nasdaq dataset.
+
+    Returns:
+        X (np.ndarray): features.
+        y (np.ndarray): ground truth.
+
+    """
+    df = pd.read_csv(input_path, nrows=250 if debug else None)
+    # X = df.iloc[:, 0:-1].values
+    X = df.loc[:, [x for x in df.columns.tolist() if x != 'NDX']].values
+    # y = df.iloc[:, -1].values
+    y = np.array(df.NDX)
+
+    return X, y
